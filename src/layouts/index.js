2561d7cc17218d65874d97f2c61d05c779f39e3d
moohooooo <suwichalala@gmail.com>
2021-02-16 01:17:02 +0700

    init

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,28 @@
-import styles from './index.css';
+import styles from './index.less';
 import "../styles/bulma.scss"
 function BasicLayout(props) {
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
+    <div id="wrapper">
+      <div id="sidebar_wrapper">
+        <div id="sidebar_heading">ZOOKEEPER!</div>
+        <div class="list_group">
+          <aside class="menu">
+            
+            <ul class="menu-list">
+              <li><a>FARM</a></li>
+              <li><a>MARKET</a></li>
+              <li><a>VOTES</a></li>
+              <li><a>FAQ</a></li>
+            </ul>
+            
+          </aside>
+        </div>
+      </div>
+
+      <div id="page_content_wrapper">
+        {props.children}
+      </div>
+
     </div>
   );
 }
