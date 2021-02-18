0a31ea375141f25c0e10d8896cf0ac5a32175d2d
moohooooo <suwichalala@gmail.com>
2021-02-18 14:48:06 +0700

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,32 @@
 import styles from './index.less';
-
-export default function() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+export default function () {
   return (
     <div className={styles.row}>
+
       <div className={styles.pool}>
-        <img src="dummy.jpg"></img>
+        <div className={styles.header}>
+          <div className={styles.title}>
+            TITLE OF THE POOL
+          </div>
+          {/*is-success for KEEPER CHOICE and is-dark for COMMUNITY CHOICE*/ }
+          <div className="choice button is-success is-outlined">
+            <span class="icon is-small">
+              <FontAwesomeIcon icon={faCheckSquare} />
+            </span>
+            <span>KEEPER CHOICE</span>
+          </div>
+        </div>
+
+
+        <div className={styles.avatar}>
+          <img src="dummy/bear.png" />
+        </div>
       </div>
-      <div className={styles.pool}>
-        <img src="dummy.jpg"></img>
-      </div>
-      <div className={styles.pool}>
-        <img src="dummy.jpg"></img>
-      </div>
-      <div className={styles.pool}>
-        <img src="dummy.jpg"></img>
-      </div>
-      <div className={styles.pool}>
-        <img src="dummy.jpg"></img>
-      </div>
-      <div className={styles.pool}>
-        <img src="dummy.jpg"></img>
-      </div>
-      <div className={styles.pool}>
-        <img src="dummy.jpg"></img>
-      </div>
-      <div className={styles.pool}>
-        <img src="dummy.jpg"></img>
-      </div>
-      
+
+
+
     </div>
   );
 }
