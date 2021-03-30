7e17d1505f45c584e6f1fb444a9a7cb236c1ef05
moohooooo <suwichalala@gmail.com>
2021-03-31 01:34:25 +0700

    add a translation id and fixed for French

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -146,8 +146,10 @@ function BasicLayout(props) {
       <StorageContext.Provider value={storage} >
         <WalletContext.Provider value={wallet} >
           <div id="page_content_wrapper">
+            <div id={getLocale()}>
             {props.children}
             <div id="blockCount">{commafy(storage.blockNumber).split('.')[0]} <div class="circle animate__animated animate__heartBeat animate__infinite"><FontAwesomeIcon icon={faCircle} /></div></div>
+            </div>
           </div>
         </WalletContext.Provider>
       </StorageContext.Provider>
