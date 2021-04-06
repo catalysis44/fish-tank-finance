5a26dfef69f454c599a387d81d3ae23386242aaa
moohooooo <suwichalala@gmail.com>
2021-04-06 11:59:57 +0700

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -213,7 +213,7 @@ function BasicLayout(props) {
                 <a href="https://github.com/zooFarming/" target="_blank"><img src="assets/social/github.svg"/></a>
               </div>
               <div className={styles.credit}>
-                ©The Wanilla {new Date().getFullYear()} / Powered by <a href="https://www.wanchain.org" target="_blank">WANCHAIN</a>
+                Powered by <a href="https://www.wanchain.org" target="_blank">WANCHAIN</a>
               </div>
             </div>
       </div>
