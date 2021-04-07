e8f1e81368673cbf45bdbfa40bd544e20d00f316
lolieatapple <lolieatapple@gmail.com>
2021-04-07 22:31:51 +0800

    fix retry of axios

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import sleep from "ko-sleep";
 
 let cache = {};
 
@@ -6,8 +7,20 @@ export const axioGet = async (url) => {
   if (cache[url]) {
     return cache[url];
   }
+  if (!url) {
+    console.error('url is undefined', url);
+    throw new Error('url is undefined');
+  }
 
-  let ret = await axios.get(url);
-  cache[url] = ret;
-  return ret;
+  let time = 300;
+  while(time-- > 0) {
+    try {
+      let ret = await axios.get(url);
+      cache[url] = ret;
+      return ret;
+    } catch (err) {
+      console.error('axioGet', err);
+      await sleep(1000);
+    }
+  }
 }
