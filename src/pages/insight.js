34461e29a0055ddb8593d1aa389281f07606838b
moohooooo <suwichalala@gmail.com>
2021-04-20 20:45:28 +0700

    scrollbar

--- src/pages/insight.js
+++ src/pages/insight.js
@@ -53,9 +53,136 @@ export default function (props) {
                     </div>
 
                     {/* Each Class of Item*/}
-                    <div className={styles.item_list}>
+                    <div className={styles.item_list_wrapper}>
+                        <div className={styles.item_list}>
+                            <div className={styles.grade}>
+                                <img src="assets/grade/N.png" /> NORMAL CLASS
+                                </div>
+                            <div className={styles.listview_table}>
+                                <div className={styles.listview_row}>
+                                    <div className={`${styles.listview_col} ${styles.header}`}>
+                                    
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.header}`}>
+                                        BOOSTER NAME
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.header}`}>
+                                        LEVEL
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.header}`}>
+                                        SUPPLY
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.header}`}>
+                                        INIT. ABILITIES
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.header}  ${styles.centered}`}>
+                                        LASTEST SOLD
+                                    </div>
+                                </div>
+                                <div className={styles.listview_row}>
+                                    <div className={`${styles.listview_col} ${styles.item_image}`}>
+                                        <img src="zoo_keeper_icons_v1/Ancient_Apple_of_Lore.png" />
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_name}`}>
+                                        Ancient Apple of Lore
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_stars}`}>
+                                        <img src="assets/star18x18.png" />
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_supply}`}>
+                                        5,555
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.item_abilities}`}>
+                                        <div><img src="assets/rocket24x24.png"/> +55.55%</div>
+                                        <div><img src="assets/hourglass24x24.png"/> -5.55%</div>
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.item_price} ${styles.centered}`}>
+                                        5,555.55$
+                                    </div>
+                                </div>
+                                <div className={styles.listview_row}>
+                                    <div className={`${styles.listview_col} ${styles.item_image}`}>
+                                        <img src="zoo_keeper_icons_v1/Tea-Smoked_Apple_Juice.png" />
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_name}`}>
+                                    Tea-Smoked Apple Juice
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_stars}`}>
+                                        <img src="assets/star18x18.png" /><img src="assets/star18x18.png" />
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_supply}`}>
+                                        5,555
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.item_abilities}`}>
+                                        <div><img src="assets/rocket24x24.png"/> +55.55%</div>
+                                        <div><img src="assets/hourglass24x24.png"/> -5.55%</div>
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.item_price}  ${styles.centered}`}>
+                                        5,555.55$
+                                    </div>
+                                </div>
+                                <div className={styles.listview_row}>
+                                    <div className={`${styles.listview_col} ${styles.item_image}`}>
+                                        <img src="zoo_keeper_icons_v1/Honeysuckle_Apple_Sauce.png" />
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_name}`}>
+                                        Honeysuckle Apple Sauce
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_stars}`}>
+                                        <img src="assets/star18x18.png" /><img src="assets/star18x18.png" /><img src="assets/star18x18.png" />
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_supply}`}>
+                                        5,555
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.item_abilities}`}>
+                                        <div><img src="assets/rocket24x24.png"/> +55.55%</div>
+                                        <div><img src="assets/hourglass24x24.png"/> -5.55%</div>
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.item_price}  ${styles.centered}`}>
+                                        5,555.55$
+                                    </div>
+                                </div>
+                                <div className={styles.listview_row}>
+                                    <div className={`${styles.listview_col} ${styles.item_image}`}>
+                                        <img src="zoo_keeper_icons_v1/Golden_Apple_of_Destiny.png" />
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_name}`}>
+                                        Golden Apple of Destiny
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_stars}`}>
+                                        <img src="assets/max.png" />
+                                    </div>
+
+                                    <div className={`${styles.listview_col} ${styles.item_supply}`}>
+                                        5,555
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.item_abilities}`}>
+                                        <div><img src="assets/rocket24x24.png"/> +55.55%</div>
+                                        <div><img src="assets/hourglass24x24.png"/> -5.55%</div>
+                                    </div>
+                                    <div className={`${styles.listview_col} ${styles.item_price}  ${styles.centered}`}>
+                                        5,555.55$
+                                    </div>
+                                </div>
+                        
+                            </div>
+                        </div>
+
+                            {/* Each Class of Item*/}
+                        <div className={styles.item_list}>
                         <div className={styles.grade}>
-                            <img src="assets/grade/N.png" /> NORMAL CLASS
+                            <img src="assets/grade/R.png" /> RARE CLASS
                             </div>
                         <div className={styles.listview_table}>
                             <div className={styles.listview_row}>
@@ -177,8 +304,7 @@ export default function (props) {
                        
                         </div>
                     </div>
-
-
+                    </div>
                 </div>
             </div>
         </React.Fragment>
